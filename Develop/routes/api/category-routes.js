3403f2874d3e86a05afbd9f9ac1b3b0ed285c674
Arnold Lee <arnoldlee60@gmail.com>
2021-07-26 21:21:35 -0500

    search by id working

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -19,12 +19,32 @@ router.get('/', (req, res) => {
     .then((CategoryData) => res.json(CategoryData)).catch((err) => {
       console.log(err);
     });
-  
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    //here
+    where: {
+      id: req.params.id, 
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((CategoryData) => {
+      //console.log(req)
+      //console.log(req.params)
+      //console.log(req.params.id)
+      res.json(CategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 router.post('/', (req, res) => {
